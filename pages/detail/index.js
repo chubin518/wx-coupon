c0d75e6b16b79c223a887c668609de77112f7771
lxw3311 <lxw3311@ly.com>
2017-08-16 00:25:54 +0800

    功能完善

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,152 +1,169 @@
 // index.js
+var productbox = require("../templates/productbox/productbox.js");
 var app = getApp();
 
 Page({
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        product: {},
-        banners: [],
-        isSuc: false,
-        content: "领取淘口令",
-        tpwd: "￥xNm40aCaJDR￥",
-        images: [],
-        showTbk: 0
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    productInfo: {},
+    isSuc: false,
+    content: "我要领券",
+    tpwd: "￥xNm40aCaJDR￥",
+    showTbk: 0,
+    curIndex: 0,
+    products: [],//推荐
+    couponInfo: {}
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        var that = this;
-        var images = [];
-        var key = options.pro;
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    productbox.init(this, []);
+    var that = this;
+    var key = options.pro;
+    wx.getStorage({
+      key: key,
+      success: function (res) {
+        var coupon = JSON.parse(res.data);
+        that.getProductImages(coupon.SPID);
+
         that.setData({
-            showTbk: app.utils.showTbk
+          couponInfo: coupon,
+          showTbk: app.utils.showTbk
         });
 
-        wx.getStorage({
-            key: key,
-            success: function(res) {
-                var product = JSON.parse(res.data);
-                that.getProductImages(product.SPID);
-
-                //设置产品名称
-                wx.setNavigationBarTitle({
-                    title: product.SPMC,
-                });
-                if (product.Images && product.Images.length >= 1) {
-                    images = product.Images;
-                }
-                images.push(product.SPZT);
-                that.setData({
-                    product: product,
-                    banners: images
-                });
-                //去除缓存
-                wx.removeStorage({
-                    key: key,
-                    success: function(res) {},
-                })
-            },
-            fail: function(res) {
-                wx.removeStorage({
-                    key: key,
-                    success: function(res) {},
-                })
-                wx.navigateBack({});
-            }
+        //去除缓存
+        wx.removeStorage({
+          key: key,
+          success: function (res) { },
+        })
+      },
+      fail: function (res) {
+        wx.removeStorage({
+          key: key,
+          success: function (res) { },
+        })
+        wx.navigateBack({});
+      }
+    });
+  },
+  getProductImages: function (pid) {
+    var that = this;
+    app.utils.doGet('detail/get', { id: pid }, function (res) {
+      if (res) {
+        that.setData({
+          productInfo: res
         });
-    },
-    getProductImages: function(pid) {
-        var that = this;
-        app.utils.doGet('search/Images', { pid: pid }, function(res) {
-            that.setData({
-                images: res.Images
-            });
+
+        //设置产品名称
+        wx.setNavigationBarTitle({
+          title: res.SPMC,
         });
-    },
-    tobuy: function() {
-        var that = this;
-        var reqParams = {
-            url: this.data.product.SPYHQTGLJ,
-            title: this.data.product.SPMC,
-            picurl: this.data.product.SPZT
-        };
-        app.utils.doGet('tpwd/Create', reqParams, function(resp) {
-            if (resp) {
-                that.setData({
-                    tpwd: resp
-                });
-            }
-            that.copyTpwd();
+      }
+    });
+  },
+  tobuy: function () {
+    var that = this;
+    var reqParams = {
+      url: this.data.product.SPYHQTGLJ,
+      title: this.data.product.SPMC,
+      picurl: this.data.product.SPZT
+    };
+    app.utils.doGet('tpwd/Create', reqParams, function (resp) {
+      if (resp) {
+        that.setData({
+          tpwd: resp
         });
-    },
-    copyTpwd: function() {
-        var that = this;
-        wx.setClipboardData({
-            data: that.data.tpwd,
-            success: function(res) {
-                that.setData({
-                    isSuc: true,
-                    content: "领取成功，请打开【手机淘宝】客户端领券购买"
-                });
-            },
-            fail: function(res) {
-                that.setData({
-                    isSuc: false,
-                    content: "领取失败请重试"
-                });
-            }
+      }
+      that.copyTpwd();
+    });
+  },
+  copyTpwd: function () {
+    var that = this;
+    wx.setClipboardData({
+      data: that.data.tpwd,
+      success: function (res) {
+        that.setData({
+          isSuc: true,
+          content: "领取成功，请打开【手机淘宝】客户端领券购买"
         });
-    },
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
+      },
+      fail: function (res) {
+        that.setData({
+          isSuc: false,
+          content: "领取失败请重试"
+        });
+      }
+    });
+  },
+  bindTap(e) {
+    var that = this;
+    var cindex = e.currentTarget.dataset.index;
+    this.setData({
+      curIndex: cindex
+    });
+    if (cindex == 1 && that.data.products.length <= 0) {
+      var reqParams = {
+        cat: this.data.productInfo.SPLM,
+      };
+      app.utils.doGet('detail/Similar', reqParams, function (resp) {
+        if (resp) {
+          that.setData({
+            products: resp.Datas
+          });
+        }
+      });
     }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 })
